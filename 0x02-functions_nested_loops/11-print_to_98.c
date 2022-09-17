bffa3c04f48e95ad5f2d02e625c35cacf9f364d6
llaurelson <llaurelson@gmail.com>
2022-09-17 03:08:02 -0700

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,6 +2,12 @@
 #include "6-abs.c"
 #include <stdio.h>
 
+/**
+ * print_to_98 - This function prints all natural numbers
+ * from n to 98, followed by a new line
+ * @n: Starting integer to count down or up to 98
+ */
+
 void print_to_98(int n)
 {
 	int i;
