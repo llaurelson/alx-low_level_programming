0edcc59a1a8f6d92a52de0e4def24109a3d08fb1
llaurelson <llaurelson@gmail.com>
2022-09-28 05:00:36 -0700

    updated

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,110 +1,59 @@
 #include "main.h"
 
 
+
 /**
 
- * wildcmp - compare two strings with "wildcard expansion" capabilities
+ * wildcmp - Compare strings
 
- * @s1: string 1
+ * @s1: pointer to string params
 
- * @s2: string 2
+ * @s2: pointer to string params
 
- * Return: 1 if strings can be considered identical, else 0
+ * Return: 0
 
- */
+ */
+
 
 
 int wildcmp(char *s1, char *s2)
 
 {
 
-    if (*s1 == '\0' && *s2 == '\0')
-
-        return (1);
-
-    else if (*s1 == '\0' || *s2 == '\0')
-
-    {
-
-        if (*s1 == '\0' && *s2 == '*')
-
-            return wildcmp(s1, ++s2);
-
-        else if (*s1 == '*' && *s2 == '\0')
-
-            return wildcmp(++s1, s2);
-
-        return (0);
-
-    }
-
-
-    if (*s1 == *s2)
-
-    {
-
-        return wildcmp(++s1, ++s2);
-
-    }
-
-    else if (*s1 == '*')
-
-    {
-
-        if (*(s1 + 1) == '*')
-
-            return wildcmp(++s1, s2);
-
-        else
-
-        {
-
-            return wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) + s2);
-
-        }
-
-    }
-
-    else if (*s2 == '*')
-
-    {
-
-        if (*(s2 + 1) == '*')
-
-            return wildcmp(s1, ++s2);
-
-        else
-
-        {
-
-            return wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2);
-
-        }
-
-    }
-
-
-    return (0);
-
-
-}
-
-
-int findsrc(char *s, char c, int i, int p)
+if (*s1 == '\0')
 
 {
 
-    if (*(s + i) == '\0')
+if (*s2 != '\0' && *s2 == '*')
 
-        return (p + 1);
+{
 
-    else if (*(s + i) == c || *(s + i) == '*')
+return (wildcmp(s1, s2 + 1));
 
-        p = i;
+}
 
-
-    return (findsrc(s, c, i + 1, p));
+return (*s2 == '\0');
 
 }
 
 
+
+if (*s2 == '*')
+
+{
+
+return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+
+}
+
+else if (*s1 == *s2)
+
+{
+
+return (wildcmp(s1 + 1, s2 + 1));
+
+}
+
+return (0);
+
+}
